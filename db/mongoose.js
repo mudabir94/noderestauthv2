34408093d48e5eafff909b00415d2741c426c3a3
mudabir94 <mudabirahmad1994@gmail.com>
2022-12-12 00:30:16 +0200

    mongoose.js updated.

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,4 +3,6 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODBURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
+}).then(
+    console.log("Connection estabilished")
+)
