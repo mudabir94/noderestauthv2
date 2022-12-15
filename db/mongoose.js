6e730774f05788f19699dad138db8a27f04ad37a
mudabir94 <mudabirahmad1994@gmail.com>
2022-12-15 20:35:19 +0200

    upadeted mongodburl variable

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(MONGODBURL, {
+mongoose.connect(process.env.MONGODBURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(
